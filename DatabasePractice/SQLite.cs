41fea75e9a83723ade2323908ed9a1130e16ecd2
kimhung.ho <kimhung.ho@arcstone.co>
2020-09-28 00:22:15 +0800

    update database lesson

---
+++ DatabasePractice/SQLite.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+
+namespace DatabasePractice
+{
+    public class SQLite
+    {
+        public SQLite(string fp)
+        {
+            if (!File.Exists(fp))
+            {
+                SQLiteConnection.CreateFile(fp);
+            }
+
+            string cs = $"Data Source={fp}";
+            string stm = "SELECT SQLITE_VERSION()";
+
+            using (SqliteConnection con = new SqliteConnection(cs))
+            {
+                con.Open();
+                using (SqliteCommand cmd = new SqliteCommand(stm, con))
+                {
+                    string version = cmd.ExecuteScalar().ToString();
+                    Console.WriteLine($"SQLite version: {version}");
+                }
+            }
+        }
+    }
+}
---
+++ PracticeHackerRank/DatabaseTest.cs
@@ -0,0 +1,18 @@
+﻿using DatabasePractice;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeHackerRank
+{
+    public class DatabaseTest
+    {
+
+        [Test]
+        public void sqliteTest()
+        {
+            SQLite sQLite = new SQLite(@"C:\Users\KimHung\Documents\Test.db");
+        }
+    }
+}
