4ae1b75aa7ad1e1756dab5bb74e473268798a04b
KimHung <kimhung.ho@titansoft.com.sg>
2021-08-16 18:18:04 +0800

    add tdd kata

---
+++ TDDKata/StringCalculator.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace TDDKata
+{
+    /// <summary>
+    /// https://osherove.com/tdd-kata-1
+    /// </summary>
+    public class StringCalculator
+    {
+    
+    }
+}
