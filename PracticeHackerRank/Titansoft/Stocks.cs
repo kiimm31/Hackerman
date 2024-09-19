0bbd23511c2fff4804a5c2bdc4467d59719e443f
Kim <kim.31@live.com.sg>
2024-09-19 22:24:04 +0800

    add ts interview question

---
+++ PracticeHackerRank/Titansoft/Stocks.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace PracticeHackerRank.Titansoft
+{
+    public class Stocks
+    {
+        [TestCase(18, 1, 10, 4, 9, 15, 13, ExpectedResult = 20)]
+        [TestCase(18, 9, 10, 4, 15, 1, 13, ExpectedResult = 24)]
+        [TestCase(9, 8, 9, 10, ExpectedResult = 2)]
+        [TestCase(1, 2, 3, 4, 5, ExpectedResult = 4)]
+
+        public int GetMaxProfit(params int[] prices)
+        {
+            int maxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                // If the price today is greater than the price yesterday, we can make a profit
+                if (prices[i] > prices[i - 1])
+                {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+
+            return maxProfit;
+        }
+
+    }
+}
---
+++ PracticeHackerRank/Titansoft/SuperMarket.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeHackerRank.Titansoft
+{
+    public class SuperMarket
+    {
+        [TestCase(1, ExpectedResult = 5)]
+        [TestCase(2, ExpectedResult = 5)]
+        [TestCase(3, ExpectedResult = 2)]
+        [TestCase(4, ExpectedResult = 5)]
+
+        public int PriceCheck(int amount)
+        {
+            var productA = new Product("A", 5, 3, 12);
+            var price = productA.GetPrice(amount);
+            return price;
+        }
+        private Cashier _cashier;
+        private Product productA;
+        private Product productB;
+        private Product productC;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cashier = new Cashier();
+            productA = new Product("A", 5, 3, 12);
+            productB = new Product("B", 4, 2, 6);
+            productC = new Product("C", 3, 0, 0);
+        }
+
+        [Test]
+        public void CashierDoWork()
+        {
+            var outputReceipt = new List<int>
+            {
+                _cashier.CheckOut(productA),
+                _cashier.CheckOut(productA),
+                _cashier.CheckOut(productB),
+                _cashier.CheckOut(productC),
+                _cashier.CheckOut(productA),
+                _cashier.CheckOut(productB)
+            };
+
+            Assert.AreEqual(21, outputReceipt.Last());
+        }
+
+
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public int SinglePrice { get; set; }
+        public int DiscountAmount { get; set; }
+        public int DiscountPrice { get; set; }
+
+        public Product(string name, int singlePrice, int discountAmount, int discountPrice)
+        {
+            Name = name;
+            SinglePrice = singlePrice;
+            DiscountAmount = discountAmount;
+            DiscountPrice = discountPrice;
+        }
+
+        private bool HasDiscount => DiscountAmount > 0 && DiscountPrice > 0;
+
+        public int GetPrice(int checkedOut)
+        {
+            if (HasDiscount && checkedOut % DiscountAmount == 0)
+            {
+                return DiscountPrice - (DiscountAmount - 1) * SinglePrice;
+            }
+            return SinglePrice;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Product product &&
+                   Name == product.Name;
+        }
+
+    }
+
+    public class Cashier
+    {
+        public int CurrentAmount { get; set; }
+        private List<Product> CheckedOutList { get; set; }
+
+        public Cashier()
+        {
+            CheckedOutList = new List<Product>();
+            CurrentAmount = 0;
+        }
+
+        public int CheckOut(Product product)
+        {
+            CheckedOutList.Add(product);
+            CurrentAmount += product.GetPrice(CheckedOutList.Count(x => x.Equals(product)));
+            return CurrentAmount;
+        }
+    }
+
+}
